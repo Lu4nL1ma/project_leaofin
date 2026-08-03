bb15e82f526ba54d3ef897123072e9dfb41a6fad
Lu4nL1ma <2019luangl@gmail.com>
2026-08-02 22:59:08 -0300

    editable

--- project_leaofin/proj_leao/app_leao/admin.py
+++ project_leaofin/proj_leao/app_leao/admin.py
@@ -19,6 +19,7 @@ class ContaPagarAdmin(admin.ModelAdmin):
     list_display = ('fornecedor', 'vencimento', 'categoria', 'banco', 'valor', 'parcela', 'status')
     list_filter = ('status', 'banco', 'categoria', 'vencimento')
     search_fields = ('fornecedor', 'nota_fiscal', 'observacao')
+    list_editable = ('status',)
     ordering = ('-vencimento',)
     date_hierarchy = 'vencimento'
 
